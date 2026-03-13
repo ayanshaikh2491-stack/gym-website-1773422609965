3a49e6b2fc6f856f3cb8e529cec2bb6f46eb6fb3
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:53:37 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422611038/components/index.tsx
@@ -0,0 +1,8 @@
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as HeroSection } from './HeroSection';
+export { default as FeaturesSection } from './FeaturesSection';
+export { default as ServicesSection } from './ServicesSection';
+export { default as TestimonialsSection } from './TestimonialsSection';
+export { default as ContactSection } from './ContactSection';
+export { default as AboutSection } from './AboutSection';
