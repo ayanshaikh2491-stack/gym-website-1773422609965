cfd0d997ad6b873bd96f619fb5fc265bd03f66bf
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:53:32 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422611038/components/Header.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link';
+
+const Nav: React.FC<{ children: any }> = ({ children }) => {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact">
+              <a>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
